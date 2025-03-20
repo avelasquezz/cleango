03341f5e4af59b9a626772d2af7ae0dfb0f93b38
avelasquezz <juanandresv2004@gmail.com>
2025-03-20 09:12:33 -0500

    Initial commit

---
+++ login-page/main.js
@@ -0,0 +1,21 @@
+let loginForm = document.getElementById('login-form');
+let registerForm = document.getElementById('register-form');
+
+let noHaveAccountButton = document.getElementById('no-account');
+let haveAccountButton = document.getElementById('have-account');
+
+noHaveAccountButton.addEventListener('click', function () {
+  loginForm.classList.add('hidden');
+  registerForm.classList.remove('hidden');
+  
+  loginForm.setAttribute("aria-hidden", "true");
+  registerForm.setAttribute("aria-hidden", "false");
+});
+
+haveAccountButton.addEventListener('click', function () {
+  registerForm.classList.add('hidden');  
+  loginForm.classList.remove('hidden');
+
+  registerForm.setAttribute("aria-hidden", "true");
+  loginForm.setAttribute("aria-hidden", "false");
+});
