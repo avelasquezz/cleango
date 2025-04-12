b8c729548b45dcc0cdea83d91f6fbedf5b2984d0
avelasquezz <juanandresv2004@gmail.com>
2025-04-12 13:47:26 -0500

    Initial commit: Base project generated by Spring Initializr

---
+++ src/main/java/com/avelasquezz/cleango/CleangoApplication.java
@@ -0,0 +1,13 @@
+package com.avelasquezz.cleango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleangoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avelasquezz/cleango/CleangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avelasquezz.cleango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
